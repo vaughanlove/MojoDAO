7cfe10547c093567229ca8524800bbc73ea2033d
von <18vel@queensu.ca>
2021-06-29 14:59:35 -0600

    ui updates and decided to make v1 using pure typescript

---
+++ mojodao-ts/sandbox.ts
@@ -0,0 +1,10 @@
+const character = 'jack';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+
+
+inputs.forEach(input =>{
+    console.log(input);
+});
---
+++ mojodao-ui/src/components/SubBox.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ba">
+    <button v-on:click="subber()">this is a test sentence</button>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line no-unused-vars
+const solanaWeb3 = require('@solana/web3.js');
+
+function subber() {
+  console.log('adad');
+}
+
+export default {
+  name: "SubBox",
+  methods: {
+    subber,
+  }
+}
+
+</script>
+
+<style scoped>
+.ba {
+  text-align: left;
+}
+</style>
